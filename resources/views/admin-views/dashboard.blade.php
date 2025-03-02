9f3147865cdbdb408721fd4f5fd5508715ce3d60
AhmadAlzo <124163736+AhmadAlzo@users.noreply.github.com>
2025-03-03 00:43:07 +0200

    push dashboard

--- resources/views/admin-views/dashboard.blade.php
+++ resources/views/admin-views/dashboard.blade.php
@@ -37,16 +37,23 @@
                     <thead class="thead-light">
                         <tr>
                             <th>{{translate('name')}}</th>
-                            <th>{{translate('address')}}</th>
+                            <th>{{translate('details')}}</th>
                             <th>{{translate('amount')}}</th>
+                            <th>{{translate('payment_type')}}</th>
+                            <th>{{translate('whatsaspp')}}</th>
                         </tr>
                     </thead>
                     <tbody id="set-rows">
                         @foreach($donates as $donate)
                             <tr>
                                 <td>{{ $donate['name'] }}</td>
-                                <td>{{ $donate['address'] }}</td>
+                                <td>
+                                    <p>{{ $donate['address'] }}</p>
+                                    <p>{{ $donate['phone'] }}</p>
+                                </td>
                                 <td>{{ $donate['amount']  }} {{ $donate['payment_currency'] == 1 ? '$' : 'LL'  }}</td>
+                                <td>{{ $donate['payment_type'] == 1 ? 'now' : 'later' }}</td>
+                                <td>{{ $donate['whatsaspp'] == 1 ? 'no' : 'yes' }}</td>
                             </tr>
                         @endforeach
                     </tbody>
--- resources/views/admin-views/partials/donates_rows.blade.php
+++ resources/views/admin-views/partials/donates_rows.blade.php
@@ -1,8 +1,13 @@
 @foreach($donates as $key => $donate)
     <tr>
         <td>{{ $donate['name'] }}</td>
-        <td>{{ $donate['address'] }}</td>
+        <td>
+            <p>{{ $donate['address'] }}</p>
+            <p>{{ $donate['phone'] }}</p>
+        </td>
         <td>{{ $donate['amount']  }} {{ $donate['payment_currency'] == 1 ? '$' : 'LL'  }}</td>
+        <td>{{ $donate['payment_type'] == 1 ? 'now' : 'later' }}</td>
+        <td>{{ $donate['whatsaspp'] == 1 ? 'no' : 'yes' }}</td>
     </tr>
 @endforeach
 
